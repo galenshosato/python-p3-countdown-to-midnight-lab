a0c8351d9b6db0578c33686cf3e0a88f4c79c75e
Galen Sho Sato <galen.sato@gmail.com>
2023-02-21 14:06:50 -0500

    Completed Lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
-# your code goes here!
+import time
+
+def countdown(start):
+    i = start
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(start):
+    i = start
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
